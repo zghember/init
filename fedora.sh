d4be89c6107b854f180ac2efe54e96a2a3e25938
Your Name <you@example.com>
2025-07-01 19:14:44 +0800

    fedora

--- fedora.sh
+++ fedora.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-systemctl enable --now sshd
-dnf -y install git tmux zsh clash exa patch g++ cmake
+dnf -y install git tmux zsh exa patch g++ cmake webkit2gtk4.1-devel gtk3-devel libappindicator-gtk3-devel librsvg2-devel patchelf @development-tools openssl-devel libxdo-devel libsoup3-devel javascriptcoregtk4.1-devel
 
 # docker
 dnf -y install docker docker-compose
@@ -17,5 +16,5 @@ dnf -y install bzip2 bzip2-devel readline-devel sqlite-devel libuuid-devel gdbm-
 pyenv install 3.8.10
 
 # jdk
-dnf -y install java-17-openjdk
+dnf -y install java-21-openjdk
 
