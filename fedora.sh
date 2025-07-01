f715c9ab5139b61afe68502a8edc6508d818f9c8
Your Name <you@example.com>
2025-07-01 23:23:35 +0800

    abc

--- fedora.sh
+++ fedora.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 # =============================== dev ==================================
-dnf -y install git vim tmux zsh patch g++ cmake webkit2gtk4.1-devel gtk3-devel libappindicator-gtk3-devel librsvg2-devel patchelf @development-tools openssl-devel libxdo-devel libsoup3-devel javascriptcoregtk4.1-devel
+dnf -y install git vim tmux zsh patch g++ cmake webkit2gtk4.1-devel gtk3-devel libappindicator-gtk3-devel librsvg2-devel patchelf @development-tools openssl-devel libxdo-devel libsoup3-devel javascriptcoregtk4.1-devel autojump-zsh java-21-openjdk mysql-server redis bzip2 bzip2-devel readline-devel sqlite-devel libuuid-devel gdbm-devel xz-devel tk-devel openssl-devel
 
-# jdk
-dnf -y install java-21-openjdk
-dnf -y install mysql-server redis
 systemctl enable --now mysqld redis
 
 # python env
 curl https://pyenv.run | bash
-dnf -y install bzip2 bzip2-devel readline-devel sqlite-devel libuuid-devel gdbm-devel xz-devel tk-devel openssl-devel
 pyenv install 3.13.5
 
 # node
