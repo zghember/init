a775a0ca96a22a18f907814695f4e587588f55f7
Your Name <you@example.com>
2025-07-01 19:34:15 +0800

    fedora

--- fedora.sh
+++ fedora.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
-dnf -y install git tmux zsh exa patch g++ cmake webkit2gtk4.1-devel gtk3-devel libappindicator-gtk3-devel librsvg2-devel patchelf @development-tools openssl-devel libxdo-devel libsoup3-devel javascriptcoregtk4.1-devel
+# =============================== dev ==================================
+dnf -y install git vim tmux zsh exa patch g++ cmake webkit2gtk4.1-devel gtk3-devel libappindicator-gtk3-devel librsvg2-devel patchelf @development-tools openssl-devel libxdo-devel libsoup3-devel javascriptcoregtk4.1-devel
 
+# jdk
+dnf -y install java-21-openjdk
+dnf -y install mysql-server redis
+systemctl enable --now mysqld redis
+
+# python env
+curl https://pyenv.run | bash
+dnf -y install bzip2 bzip2-devel readline-devel sqlite-devel libuuid-devel gdbm-devel xz-devel tk-devel openssl-devel
+pyenv install 3.13.5
+
+# node
+# Download and install fnm:
+curl -o- https://fnm.vercel.app/install | bash
+# Download and install Node.js:
+fnm install 22
+
+# rust & cargo
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# =============================== server ==================================
 # docker
 dnf -y install docker docker-compose
 systemctl enable --now docker
@@ -10,11 +31,4 @@ dnf -y install nginx
 systemctl enable --now nginx
 setsebool -P httpd_can_network_connect 1
 
-# python env
-curl https://pyenv.run | bash
-dnf -y install bzip2 bzip2-devel readline-devel sqlite-devel libuuid-devel gdbm-devel xz-devel tk-devel openssl-devel
-pyenv install 3.8.10
-
-# jdk
-dnf -y install java-21-openjdk
 
